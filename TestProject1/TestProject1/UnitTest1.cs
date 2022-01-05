006eda89197fe45d80cf20d5df7e4d919b03ba90
mpushnikA1QA <m.pyshnik@a1qa.com>
2022-01-05 10:02:45 +0300

    Update some code

--- TestProject1/TestProject1/UnitTest1.cs
+++ TestProject1/TestProject1/UnitTest1.cs
@@ -1,5 +1,9 @@
+using Aquality.Selenium.Browsers;
 using NUnit.Framework;
 using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
 
 namespace TestProject1
 {
@@ -8,8 +12,24 @@ namespace TestProject1
         [Test]
         public void Test1()
         {
+            var commonPathToBinFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            string fullPathForSavedImage = $@"{commonPathToBinFolder}\tetsImge.jpg";
             string envValue = Environment.GetEnvironmentVariable("tetsUrl");
             Assert.AreEqual("https://learn.javascript.ru/", envValue);
+
+            AqualityServices.Browser.GoTo(envValue);
+            AqualityServices.Browser.Maximize();
+
+            var bytes = AqualityServices.Browser.GetScreenshot();
+
+            using (var streamBitmap = new MemoryStream(bytes))
+            {
+                var img = Image.FromStream(streamBitmap);
+                img.Save(fullPathForSavedImage);
+            }
+
+            AqualityServices.Browser.Quit();
         }
     }
 }
